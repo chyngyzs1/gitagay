2171eee5addd28fca941aaf8b9be138d581c8df2
chyngyzs1 <chyngyz.9821@gmail.com>
2019-11-25 10:20:23 +0600

    Coding is easy

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<!-- CSS файлы -->
+	<link rel="stylesheet" type="text/css" href="css/style_for_index.css">
+	<!-- Шрифты -->
+	<!-- Kaushan Script -->
+	<link href="https://fonts.googleapis.com/css?family=Kaushan+Script&display=swap" rel="stylesheet">
+	<!-- JS -->
+	<script src="js/js_for_index.js" defer></script>
+	<script src="js/font_awesome.js" defer></script>
+</head>
+<body>
+
+<?php require_once "block/header.php" ?>
+<h1>Main</h1>
+
+<?php require "block/footer.php" ?>
+<?php require "block/footer.php" ?>
+<?php require "block/footer.php" ?>
+
+
+</body>
+</html> 
